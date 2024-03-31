c8c560fc8e27b2c987e003242ce29ce8e54f414a
Abhay Agrawal <agrawalabhay45@gmail.com>
2024-04-01 00:40:41 +0530

    just for fun

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Let's write some javaScript");
 async function main(){
-    let a= await fetch("http://127.0.0.1:5500/Spotify%20Project/Songs/");
+    let a= await fetch("http://127.0.0.1:5500/HarryExercises/Spotify%20Project/Songs/");
     let response=await a.text();
     console.log(response);
     
